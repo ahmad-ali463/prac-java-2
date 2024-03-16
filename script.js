6e60c35c4e72534e5b972029b59d9a3d6db287dc
Ahmad Ali Asif <spyali463@gmail.com>
2024-03-16 18:07:53 +0500

    prac

---
+++ script.js
@@ -0,0 +1,49 @@
+
+alert("Ahmad Ali Asif");
+
+
+
+document.getElementById("aa").onclick= function(){
+    let num1 = 10;
+    let num2 = 5;
+    let gg = num1 + num2;
+    document.getElementById("gg").innerHTML= gg;
+}
+
+
+
+document.getElementById("con").onclick= function(){
+    let gg = "<p><ul><li>this is variable</li><li>this is another variable</li></ul></p>"
+    document.getElementById("gg").innerHTML= gg;
+}
+
+
+document.getElementById("al").onclick= function(){
+    alert('ahmad')
+    let gg = "alert('ahmad')"
+    document.getElementById("gg").innerHTML= gg;
+}
+
+
+document.getElementById("ahmad").onclick= function(){
+    alert('111')
+    let gg = "alert('897')"
+    document.getElementById("gg").innerHTML= gg;
+}
+
+
+
+let num3 = 50;
+let num4 = 10;
+let multiply = num3 * num4;
+console.log(multiply);
+
+
+
+
+
+
+let firstName = "Ahmad"
+let lastName = "Ali"
+let fullName = firstName +" "+ lastName;
+console.log(fullName);
